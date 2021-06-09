9ee6a62c04876fe333a712bec8f427d72762d96e
lwlsimm <77008736+lwlsimm@users.noreply.github.com>
2021-06-09 11:23:32 +0100

    add deploy config

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-// const devConfig = {
-//   user: process.env.SERVER_USER,
-//   password: process.env.SERVER_PASSWORD,
-//   host: process.env.SERVER_HOST,
-//   port: process.env.SERVER_PORT,
-//   database: process.env.SERVER_DATABASE
-// }
-
 const devConfig = {
-  user: 'postgres',
-  password: '2151',
-  host: 'localhost',
-  port: 5432,
-  database: 'edward_and_co'
+  user: process.env.SERVER_USER,
+  password: process.env.SERVER_PASSWORD,
+  host: process.env.SERVER_HOST,
+  port: process.env.SERVER_PORT,
+  database: process.env.SERVER_DATABASE
 }
 
 const proConfig = {
