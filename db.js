1ac774e2759efb4de26681e1b5bd53004fab7aed
lwlsimm <77008736+lwlsimm@users.noreply.github.com>
2021-06-09 12:15:53 +0100

    added

--- db.js
+++ db.js
@@ -13,6 +13,6 @@ const proConfig = {
   connectionString: process.env.DATABASE_URL
 }
 
-const pool = new Pool( proConfig )
+const pool = new Pool( process.env.NODE_ENV === 'production' ?  proConfig : devConfig )
 
 module.exports = pool;
