211d376bea503585e7e24111e69fea72ece02509
lwlsimm <77008736+lwlsimm@users.noreply.github.com>
2021-06-09 13:03:47 +0100

    dev testing

--- functions/registrationFunctions.js
+++ functions/registrationFunctions.js
@@ -14,9 +14,10 @@ const checkEmailExists = async (email) => {
 
 const registerUser = async(data, password) => {
  const { first_name, last_name, email, address_1, address_2, postal_town, post_code, country } = data;
- 
+ console(`Step 3: email ${email}, first_name ${first_name}, last_name ${last_name}, password ${password}.`)
  const customerData = await pool.query("INSERT INTO customers (email, first_name, last_name, password) VALUES($1, $2, $3, $4) RETURNING id", [email, first_name, last_name, password]);
  const customer_id = await customerData.rows[0].id;
+ console.log(`Step 4: customer data ${customer_id}`)
  const addressData = await pool.query("INSERT INTO addresses (customer_id, address_1, address_2, postal_town, post_code, country) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id", [customer_id, address_1, address_2, postal_town, post_code, country]);
  const address_id = await addressData.rows[0].id;
   if(address_id) {
